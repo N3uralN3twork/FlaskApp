05263081cc0760bfd0fbd79c543b22249d359f3e
Matthias Quinn <53357992+N3uralN3twork@users.noreply.github.com>
2024-09-24 07:09:38 -0400

    Add plotly graphics

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -1,9 +1,11 @@
+import json
 from apps.home import blueprint
 from flask import render_template, request
 from flask_login import login_required
 from jinja2 import TemplateNotFound
 import pandas as pd
-
+from plotly.utils import PlotlyJSONEncoder
+import plotly.graph_objects as go
 
 def extract_after_tree(url, substring):
     # Find the position of "tree/" in the URL
@@ -25,6 +27,18 @@ dfBL["branch_url"] = dfBL["Branch"]
 dfBL["Branch"] = dfBL["Branch"].apply(extract_after_tree, substring="tree")
 dfBL["Commit"] = dfBL["Commit"].apply(extract_after_tree, substring="commit")
 
+def create_plot():
+    fig =go.Figure(go.Icicle(
+        labels=["Eve", "Cain", "Seth", "Enos", "Noam", "Abel", "Awan", "Enoch", "Azura"],
+        parents=["", "Eve", "Eve", "Seth", "Seth", "Eve", "Eve", "Awan", "Eve" ],
+        values=[10, 14, 12, 10, 2, 6, 6, 4, 4],
+        root_color="lightgrey"
+    ))
+
+    fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
+
+    plot_json = json.dumps(fig, cls=PlotlyJSONEncoder)
+    return plot_json
 
 @blueprint.route('/index')
 @login_required
@@ -44,15 +58,17 @@ def route_template(template):
         # Detect the current page
         segment = get_segment(request)
 
-        if not template.endswith("models.html"):
-            # Serve the file (if exists) from app/templates/home/FILE.html
-            return render_template("home/" + template, segment=segment)
-        elif template.endswith("landing.html"):
+        if template.endswith("landing.html"):
             return render_template("home/landing.html")
         elif template.endswith("demo.html"):
             return render_template("home/demo.html", df=dfBL)
-        else:
+        elif template.endswith("models.html"):
             return render_template("home/models.html", df=dfBL)
+        elif template.endswith("tree.html"):
+            plot = create_plot()
+            return render_template("home/tree.html", df=dfBL, plot=plot)
+        else:
+            return render_template("home/" + template, segment=segment)
 
     except TemplateNotFound:
         return render_template('home/page-404.html'), 404
